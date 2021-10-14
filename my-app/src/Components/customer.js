95b50ad44237cfbb21e78cca34fb455d3bc71019
include-jacopo <jacopopati@gmail.com>
2021-10-14 21:06:15 +0200

    added customer interface and images

--- my-app/src/Components/customer.js
+++ my-app/src/Components/customer.js
@@ -1,74 +1,61 @@
 import { Col, Row, Container } from 'react-bootstrap';
 import { Button, Card } from 'react-bootstrap';
 import API from '../API.js';
+import "./customer.css";
 
 function Customer(props) {
 
-    function handleClick(serviceType){
+    function handleClick(serviceType) {
         API.getTicket(serviceType);
     }
 
     return (
+        <Container>
 
-        <Container className="d-flex justify-content-center">
-        <Col>
-            <Row>
-                <Card>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>SERVICE 1</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary" onClick={()=>handleClick("service1")}>Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+            <Row className="justify-content-center mt-3 text">GET YOUR TICKET</Row>
+
+            <Row className="justify-content-center mt-3">
+                <Col xs lg="4">
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>SERVICE 1</Card.Title>
+                            <Button variant="primary" onClick={() => handleClick("service1")}>Add me to queue</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col xs lg="4">
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>SERVICE 2</Card.Title>
+                            <Button variant="primary" onClick={() => handleClick("service2")}>Add me to queue</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
             </Row>
-            <Row>
-            <Card>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>SERVICE 2</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary" onClick={()=>handleClick("service2")}>Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+            <Row className="justify-content-center mt-2 ml-2">
+                <Col xs lg="4">
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>SERVICE 3</Card.Title>
+                            <Button variant="primary" onClick={() => handleClick("service3")}>Add me to queue</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col xs lg="4">
+                    <Card>
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Title>SERVICE 4</Card.Title>
+                            <Button variant="primary" onClick={() => handleClick("service4")}>Add me to queue</Button>
+                        </Card.Body>
+                    </Card>
+                </Col>
             </Row>
-        </Col>
-        <Col>
-         <Row>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>SERVICE 3</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary" onClick={()=>handleClick("service3")}>Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </Row>
-            <Row>
-            <Card>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>SERVICE 4</Card.Title>
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </Card.Text>
-                        <Button variant="primary" onClick={()=>handleClick("service4")}>Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-            </Row>
-        </Col>
-    </Container>
-        
+        </Container>
+
     );
 };
 
