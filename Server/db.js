a39aa9646391904867cc812cb2a9f16da6345cdf
Alessio Magurno <s290234@studenti.polito.it>
2021-10-14 22:34:11 +0200

    prova 2

--- Server/db.js
+++ Server/db.js
@@ -3,7 +3,7 @@
 const sqlite = require('sqlite3');
 
 // open the database
-const db = new sqlite.Database('selectionType.sqlite', (err) => {
+const db = new sqlite.Database('OQM.sql', (err) => {
   if (err) throw err;
 });
 
