497938a8b566ce946638dc24d73bebaf6c81bc8e
taurou <80039469+taurou@users.noreply.github.com>
2021-10-14 18:43:08 +0200

    Implementing router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,12 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Select from 'react-select'
+import { Button, DropdownButton, Dropdown } from 'react-bootstrap'
 import axios from 'axios'
 import API from './API';
-
+import NavigationBar from './ManagerView';
+/* 
 export default class App extends Component {
 
   constructor(props){
@@ -53,7 +56,48 @@ export default class App extends Component {
       <div>
         <Select options={this.state.selectOptions} onChange={this.handleChange.bind(this)} />
     <p>You have selected <strong>{this.state.name}</strong> whose id is <strong>{this.state.id}</strong></p>
+    <Button>ahaadd</Button>
+    <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+  <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+  <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+</DropdownButton>
       </div>
     )
   }
+} */
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Button>Pagina/</Button>
+          </Route>
+          <Route exact path="/andrea">
+            <Button>PaginaAndrea</Button>
+          </Route>
+          <Route exact path="/bruno">
+
+            <Button>PaginaBruno</Button>
+          </Route>
+          <Route exact path="/jacopo">
+
+            <Button>PaginaJacopo</Button>
+          </Route>
+        </Switch>
+
+
+
+      </Router>
+
+    </>
+  );
+
+
 }
+
+
+export default App;
